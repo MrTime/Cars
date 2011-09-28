bb2f6107593332b6a8dc81edacbb9c1da5db5234
MrTime <kfedia@gmail.com>
2011-09-28 15:36:15 +0300

    hide windows console

--- src/cars/car_test.cpp
+++ src/cars/car_test.cpp
@@ -8,16 +8,17 @@
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
 using namespace irr;
 using namespace core;
 using namespace gui;
 
-int main(int argc, char* argv[])
+int main()
 {
 	// init Irrlicht
-	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640,480), 16, false, false, false);
+	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(1024,768), 16, false, false, false);
 	if (!device)
 		return 1;
 
